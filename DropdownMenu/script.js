af4c88ee2157e71df8a7f95179c066ad3c6e2f77
mohamedniang <33057571+mohamedniang@users.noreply.github.com>
2019-04-22 15:37:17 +0000

    These files are made for Yembi Yembi Yembi x3

---
+++ DropdownMenu/script.js
@@ -0,0 +1,16 @@
+var rippleBtns = document.querySelectorAll('.ripple');
+
+rippleBtns.forEach(rippleBtn => {
+	rippleBtn.addEventListener('click', e => {
+		if (!rippleBtn.classList.contains('rippling')) {
+			rippleBtn.classList.add('rippling');
+			rippleBtn.addEventListener('animationend', e => {
+				rippleBtn.classList.add('fade');
+				rippleBtn.classList.remove('rippling');
+				setTimeout(() => {
+					rippleBtn.classList.remove('fade');
+				}, 300);
+			});
+		}
+	});
+});
