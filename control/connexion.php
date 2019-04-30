645df30e678996b9a5151d91d2ed0d31a6a03386
Mohamed Niang <mouhamedniang1997@gmail.com>
2019-04-30 21:33:10 +0000

    no msg

--- control/connexion.php
+++ control/connexion.php
@@ -2,8 +2,8 @@
 		
 	$host = 'localhost';
 	$dbname = 'databank';
-	$dbuser = 'root';
-	$dbpwd = '';
+	$dbuser = 'phpmyadmin';
+	$dbpwd = 'admin';
 
 	try{
 		$conx = new PDO('mysql:host='.$host.'; dbname='.$dbname,$dbuser,$dbpwd,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
--- pages/soutenance.php
+++ pages/soutenance.php
@@ -1,3 +1,4 @@
-<div class="container">
-    <span class="titre"> <i class="fas fa-exclamation-triangle"></i> Section en dev</span>
+<div class="container-fluid" style="text-align:center;">
+    <h1 class="titre"> <i class="fas fa-exclamation-triangle" style="color:#e4c721;"></i> Section en développement</h1>
+    <h2 class="demiTitre">Disponible bientôt</h2>
 </div>
